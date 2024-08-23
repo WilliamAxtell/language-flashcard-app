2ffb04daad7e3fb5f614e9b53265fe4b4bd9fd0c
WilliamAxtell <111174139+WilliamAxtell@users.noreply.github.com>
2024-08-23 16:43:58 +0100

    Card Started

--- flashcard-frontend/src/App.vue
+++ flashcard-frontend/src/App.vue
@@ -5,14 +5,6 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/Flag_of_Norway.png" width="154" height="112" />
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 
   <RouterView />
@@ -52,32 +44,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- flashcard-frontend/src/views/Flashcards.vue
+++ flashcard-frontend/src/views/Flashcards.vue
@@ -1,25 +1,67 @@
 <script setup>
   import { wordStore } from '../../stores/words.js'
-  import { ref } from 'vue'
+  import { onMounted, ref } from 'vue'
 
   const store = wordStore()
-
   const sharedData = JSON.parse(store.sharedData)
-
   const workingWords = ref(sharedData);
+  let randomNum;
 
   const updateData = () => {
     store.setSharedData('New Value')
   }
+
+  onMounted(() => {
+    randomNum = Math.floor(Math.random() * sharedData.length);
+    workingWords.value = sharedData[randomNum];
+  })
+
+  const checkAnswer = () => {
+    if (document.getElementById('card-answer').value === '') {
+      alert('Please enter an answer');
+      return;
+    }
+    const answer = workingWords.value.meaning;
+    const transRegex = new RegExp(document.getElementById('card-answer').value, 'i');
+    transRegex.test(answer) ? correctAnswer() : incorrectAnswer();
+  }
+  
+  const correctAnswer = () => {
+    alert('Correct!');
+    sharedData.splice(randomNum, 1);
+    if (sharedData.length === 0) {
+      alert('You have completed all the words!');
+    } else {
+      randomNum = Math.floor(Math.random() * sharedData.length);
+      workingWords.value = sharedData[randomNum];
+    }
+  }
+
+  const incorrectAnswer = () => {
+    alert('Incorrect!');
+  }
+
 </script>
 
 <template>
-    <div class="about">
-      <h1>Hei!</h1>
-      <ul>
-        <li v-for="item in workingWords">
-          {{ item }}
-        </li>
-      </ul>
+    <div class="card">
+      <h1>{{ workingWords.word }}</h1>
+      <input type="text" id="card-answer"/>
+      <button @click="checkAnswer">Update Data</button>
     </div>
 </template>
+
+<style>
+
+.card {
+  display: grid;
+
+  margin: 0 auto;
+  width: 80%;
+  aspect-ratio: 3/2;
+  border: 1px solid black;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+</style>
