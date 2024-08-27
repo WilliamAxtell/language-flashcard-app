3229a5d0561244c0ec5ee7eaa3c1c049db1baef4
WilliamAxtell <111174139+WilliamAxtell@users.noreply.github.com>
2024-08-27 17:10:36 +0100

    Updates

--- flashcard-frontend/src/views/Flashcards.vue
+++ flashcard-frontend/src/views/Flashcards.vue
@@ -5,6 +5,9 @@
   const store = wordStore()
   const sharedData = JSON.parse(store.sharedData)
   const workingWords = ref(sharedData);
+  const cardDisplay = ref("Loading...");
+  const warning = ref(false);
+  const buttonOption = ref(true);
   let randomNum;
 
   const updateData = () => {
@@ -12,42 +15,54 @@
   }
 
   onMounted(() => {
-    randomNum = Math.floor(Math.random() * sharedData.length);
-    workingWords.value = sharedData[randomNum];
+    setWord();
   })
 
-  const checkAnswer = () => {
-    if (document.getElementById('card-answer').value === '') {
-      alert('Please enter an answer');
-      return;
-    }
-    const answer = workingWords.value.meaning;
-    const transRegex = new RegExp(document.getElementById('card-answer').value, 'i');
-    transRegex.test(answer) ? correctAnswer() : incorrectAnswer();
+  const setWord = () =>{
+    buttonOption.value = true;
+    randomNum = Math.floor(Math.random() * sharedData.length);
+    workingWords.value = sharedData[randomNum];
+    cardDisplay.value = workingWords.value.word;
   }
   
-  const correctAnswer = () => {
-    alert('Correct!');
+  const checkAnswer = () => {
+    if (document.getElementById('card-answer').value === '') {
+      warning.value = true;
+      return;
+    }
+    buttonOption.value = false;
+    warning.value = false;
+    const answer = workingWords.value.meaning;
+    const transRegex = new RegExp(document.getElementById('card-answer').value, 'i');
+
+    if(transRegex.test(answer)) {
+      cardDisplay.value = `Correct Answer: ${workingWords.value.meaning}`;
+    } else {
+      cardDisplay.value = `Incorrect Answer: ${workingWords.value.meaning}`;
+    }
+    
     sharedData.splice(randomNum, 1);
     if (sharedData.length === 0) {
-      alert('You have completed all the words!');
+      cardDisplay.value = `You have completed all the words!`;
     } else {
       randomNum = Math.floor(Math.random() * sharedData.length);
       workingWords.value = sharedData[randomNum];
     }
-  }
 
-  const incorrectAnswer = () => {
-    alert('Incorrect!');
+    document.getElementById('card-answer').value = '';
   }
 
 </script>
 
 <template>
     <div class="card">
-      <h1>{{ workingWords.word }}</h1>
-      <input type="text" id="card-answer"/>
-      <button @click="checkAnswer">Update Data</button>
+      <h1>{{ cardDisplay }}</h1>
+      <p v-if="warning" class="card-warning">Please enter the answer below</p>
+      <input v-if="buttonOption" type="text" id="card-answer"/>
+      <div>
+        <button v-if="buttonOption" @click="checkAnswer">Check Answer</button>
+        <button v-else @click="setWord">Next Word</button>
+      </div>
     </div>
 </template>
 
@@ -62,6 +77,11 @@
   border: 1px solid black;
   border-radius: 5px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.card-warning {
+  color: red;
+  font-style: italic;
 }
 
 </style>
